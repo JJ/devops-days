09a582de38c511df9ac3050887828a41ac44ab73
Javier Carrillo <stringparser@gmail.com>
2015-04-21 00:21:02 +0200

    indentar con dos espacios?, comentario en la ultima tarea definida

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,165 +1,166 @@
-/* global module:false */
+'use strict';
+
 module.exports = function(grunt) {
-    require('load-grunt-tasks')(grunt);
+  require('load-grunt-tasks')(grunt);
 
-    var port = grunt.option('port') || 8000;
-    // Project configuration
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        meta: {
-            banner:
-            '/*!\n' +
-                ' * DevOpsOSSAzureDays presentation <%= pkg.version %> (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
-                ' * http://github.com/JJ/devops-days\n' +
-                ' * Apache licensed\n' +
-                ' *\n' +
-                ' * Copyright (C) 2015 Hakim El Hattab, http://hakim.se\n' +
-                ' * Copyright (C) 2015 JJ Merelo, http://jj.github.io (nuevo contenido)\n' +
-                ' */'
-        },
+  var port = grunt.option('port') || 8000;
+  // Project configuration
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    meta: {
+      banner:
+        '/*!\n' +
+        ' * DevOpsOSSAzureDays presentation <%= pkg.version %>' +
+        ' (<%= grunt.template.today("yyyy-mm-dd, HH:MM") %>)\n' +
+        ' * http://github.com/JJ/devops-days\n' +
+        ' * Apache licensed\n' +
+        ' *\n' +
+        ' * Copyright (C) 2015 Hakim El Hattab, http://hakim.se\n' +
+        ' * Copyright (C) 2015 JJ Merelo, http://jj.github.io' +
+        ' (nuevo contenido)\n' +
+        ' */'
+    },
 
-        qunit: {
-            files: [ 'test/*.html' ]
-        },
+    qunit: {
+      files: [ 'test/*.html' ]
+    },
 
-        sass: {
-            core: {
-                files: {
-                    'css/reveal.css': 'css/reveal.scss',
-                }
-            },
-            themes: {
-                files: {
-                    'css/theme/black.css': 'css/theme/source/black.scss',
-                    'css/theme/white.css': 'css/theme/source/white.scss',
-                    'css/theme/league.css': 'css/theme/source/league.scss',
-                    'css/theme/beige.css': 'css/theme/source/beige.scss',
-                    'css/theme/night.css': 'css/theme/source/night.scss',
-                    'css/theme/serif.css': 'css/theme/source/serif.scss',
-                    'css/theme/simple.css': 'css/theme/source/simple.scss',
-                    'css/theme/sky.css': 'css/theme/source/sky.scss',
-                    'css/theme/moon.css': 'css/theme/source/moon.scss',
-                    'css/theme/solarized.css': 'css/theme/source/solarized.scss',
-                    'css/theme/blood.css': 'css/theme/source/blood.scss'
-                }
-            }
-        },
+    sass: {
+      core: {
+        files: {
+          'css/reveal.css': 'css/reveal.scss',
+        }
+      },
+      themes: {
+        files: {
+          'css/theme/black.css': 'css/theme/source/black.scss',
+          'css/theme/white.css': 'css/theme/source/white.scss',
+          'css/theme/league.css': 'css/theme/source/league.scss',
+          'css/theme/beige.css': 'css/theme/source/beige.scss',
+          'css/theme/night.css': 'css/theme/source/night.scss',
+          'css/theme/serif.css': 'css/theme/source/serif.scss',
+          'css/theme/simple.css': 'css/theme/source/simple.scss',
+          'css/theme/sky.css': 'css/theme/source/sky.scss',
+          'css/theme/moon.css': 'css/theme/source/moon.scss',
+          'css/theme/solarized.css': 'css/theme/source/solarized.scss',
+          'css/theme/blood.css': 'css/theme/source/blood.scss'
+        }
+      }
+    },
 
-        autoprefixer: {
-            dist: {
-                src: 'css/reveal.css'
-            }
-        },
+    autoprefixer: {
+      dist: {
+          src: 'css/reveal.css'
+      }
+    },
 
-        cssmin: {
-            compress: {
-                files: {
-                    'css/reveal.min.css': [ 'css/reveal.css' ]
-                }
-            }
-        },
+    cssmin: {
+      compress: {
+        files: {
+            'css/reveal.min.css': [ 'css/reveal.css' ]
+        }
+      }
+    },
 
-        jshint: {
-            options: {
-                curly: false,
-                eqeqeq: true,
-                immed: true,
-                latedef: true,
-                newcap: true,
-                noarg: true,
-                sub: true,
-                undef: true,
-                eqnull: true,
-                browser: true,
-                expr: true,
-                globals: {
-                    head: false,
-                    module: false,
-                    console: false,
-                    unescape: false,
-                    define: false,
-                    exports: false
-                }
-            },
-            files: [ 'Gruntfile.js', 'js/reveal.js' ]
-        },
+    jshint: {
+      options: {
+        curly: false,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        eqnull: true,
+        browser: true,
+        expr: true,
+        globals: {
+            head: false,
+            module: false,
+            console: false,
+            unescape: false,
+            define: false,
+            exports: false
+        }
+      },
+      files: [ 'Gruntfile.js', 'js/reveal.js' ]
+    },
+    connect: {
+      server: {
+        options: {
+          port: port,
+          base: '.',
+          livereload: true,
+          open: true
+        }
+      }
+    },
 
-        connect: {
-            server: {
-                options: {
-                    port: port,
-                    base: '.',
-                    livereload: true,
-                    open: true
-                }
-            }
-        },
+    zip: {
+      'reveal-js-presentation.zip': [
+        'index.html',
+        'css/**',
+        'js/**',
+        'lib/**',
+        'images/**',
+        'plugin/**'
+      ]
+    },
 
-        zip: {
-            'reveal-js-presentation.zip': [
-                'index.html',
-                'css/**',
-                'js/**',
-                'lib/**',
-                'images/**',
-                'plugin/**'
-            ]
-        },
+    watch: {
+      options: {
+        livereload: true
+      },
+      js: {
+        files: [ 'Gruntfile.js', 'js/reveal.js' ],
+        tasks: 'js'
+      },
+      theme: {
+        files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
+        tasks: 'css-themes'
+      },
+      css: {
+        files: [ 'css/reveal.scss' ],
+        tasks: 'css-core'
+      },
+      html: {
+        files: [ 'index.html']
+      }
+    },
+    shell: {
+      options: {
+        stderr: false
+      },
+      target: {
+        command: 'ls'
+      }
+    }
 
-        watch: {
-            options: {
-                livereload: true
-            },
-            js: {
-                files: [ 'Gruntfile.js', 'js/reveal.js' ],
-                tasks: 'js'
-            },
-            theme: {
-                files: [ 'css/theme/source/*.scss', 'css/theme/template/*.scss' ],
-                tasks: 'css-themes'
-            },
-            css: {
-                files: [ 'css/reveal.scss' ],
-                tasks: 'css-core'
-            },
-            html: {
-                files: [ 'index.html']
-            }
-        },
+  });
 
-  shell: {
-            options: {
-    stderr: false
-            },
-            target: {
-    command: 'ls' //'echo '+process.env.AZ_USERNAME+" "+process.env.AZ_PASSWD
-            }
-  }
+  // Default task
+  grunt.registerTask( 'default', [ 'css', 'js' ] );
 
-    });
+  // Theme CSS
+  grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
 
-    // Default task
-    grunt.registerTask( 'default', [ 'css', 'js' ] );
+  // Core framework CSS
+  grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
 
-    // Theme CSS
-    grunt.registerTask( 'css-themes', [ 'sass:themes' ] );
+  // All CSS
+  grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
 
-    // Core framework CSS
-    grunt.registerTask( 'css-core', [ 'sass:core', 'autoprefixer', 'cssmin' ] );
+  // Package presentation to archive
+  grunt.registerTask( 'package', [ 'default', 'zip' ] );
 
-    // All CSS
-    grunt.registerTask( 'css', [ 'sass', 'autoprefixer', 'cssmin' ] );
+  // Serve presentation locally
+  grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
 
-    // Package presentation to archive
-    grunt.registerTask( 'package', [ 'default', 'zip' ] );
+  // Run tests
+  grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
 
-    // Serve presentation locally
-    grunt.registerTask( 'serve', [ 'connect', 'watch' ] );
-
-    // Run tests
-    grunt.registerTask( 'test', [ 'jshint', 'qunit' ] );
-
-    // Run tests
-    grunt.registerTask( 'vm', [ 'shell' ] );
+  // Run vm
+  grunt.registerTask( 'vm', [ 'shell' ] );
 
 };
