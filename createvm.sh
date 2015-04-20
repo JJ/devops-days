99b32eddd8137fa7a5bae33f02769bb644a53247
JJ Merelo <jjmerelo@gmail.com>
2015-04-20 07:49:09 +0200

    Final (maybe) script for Azure

--- createvm.sh
+++ createvm.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-#Disk must be created in advance
-azure vm create-from devops-days stuff/devops-days.json --location "West Europe"
+#Call with username and password
+azure vm create devops-days -o vmdepot-51465-1-32 -l "West Europe" $1 $2 --ssh
+azure vm endpoint create-multiple devops-days 80:80,443:443
+ssh-copy-id -i ~/.ssh/id_rsa.pub $1@devops-days.cloudapp.net
